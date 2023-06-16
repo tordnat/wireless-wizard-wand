60b917b6ed7b36821413db74f3e69fb53ef49914
Tord Natlandsmyr <tordna@protonmail.com>
2023-06-16 16:35:15 +0200

    Change to CPP 😐

---
+++ src/include/imu.h
@@ -0,0 +1,7 @@
+
+// Static variables
+#define SAMPLING_FREQ 101
+#define TIME_BETWEEN_SAMPLES_US (1000000 / (SAMPLING_FREQ - 1))
+  
+void imu_get_acceleration(struct sensor_value* value);
+void imu_init();
