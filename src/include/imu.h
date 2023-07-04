eb06a7ef25a4ddaa86c05f01057d4a2d50dde437
Tord Natlandsmyr <tordna@protonmail.com>
2023-07-04 17:09:05 +0200

    Fix: Correct sample rate (things run on time now) 🥳

--- src/include/imu.h
+++ src/include/imu.h
@@ -2,8 +2,8 @@
 #define _IMU_H_
 
 // Globals
-#define SAMPLING_FREQ 101 //Hz
-#define TIME_BETWEEN_SAMPLES_US (1000000 / (SAMPLING_FREQ - 1)) //microseconds
+#define SAMPLING_FREQ 100 //Hz
+#define TIME_BETWEEN_SAMPLES_US (1000000 / (SAMPLING_FREQ)) //microseconds
 #define IMU_AXIS_SAMPLED 3
 #define SHARED_BUFFER_MUTEX_TIMEOUT 100 //ms
 
